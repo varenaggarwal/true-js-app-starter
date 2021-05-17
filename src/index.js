86c0085eee4305921e6fa970f5f45013504ac351
Varen Aggarwal <varenaggarwal@gmail.com>
2021-05-17 16:15:26 +0530

    inital commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const root = document.getElementById("root");
+
+function init() {
+  root.innerHTML = `
+        <h1> Welcome to Vanila JS Starter app</h1>
+        <h2> Do your magic!</h2>
+        `;
+}
+
+init();
